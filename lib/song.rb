4953f896ea767b86aedf6db1f21152b36cc02107
Joaquín Herrera <herrerab.joaquin@gmail.com>
2018-06-25 20:00:54 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -51,7 +51,6 @@ class Song
       song = Song.new(file[1])
       song.artist = Artist.find_or_create_by_name(file[0])
       song.genre = Genre.find_or_create_by_name(file[2])
-      song.save
       song
   end
 
